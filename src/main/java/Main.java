bcd63735bc983686d060c7bf2f8de9799fcbc258
Alekseyka <harlan14@mail.ru>
2025-09-05 00:28:02 +0700

    second and third class

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.birthday.day = "13";
+        post.birthday.month = "6";
+        post.birthday.year = "1999";
+
+        post.name = "Иван";
+        post.passport = "444444444444";
+        post.patronymic = "Иванович";
+        post.pone = "8_923_554_4450";
+        post.surname = "Иванов";
+        post.subscription = true;
+    }
+
+}
+
+
+
+
+
+
+
+
